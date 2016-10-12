54508726ff70de66a59e4d2faaf763b04543d9d2
timer84 <violaleafs@gmail.com>
2016-10-11 23:44:56 -0400

    First commit to dev

---
+++ Test/Panda-Core/Security.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Panda_Core
+{
+    [DataContract]
+    public class Security
+    {
+        [DataMember]
+        public string SecId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Exchange { get; set; }
+
+        [DataMember]
+        public string Type { get; set; }
+    }
+}
---
+++ Test/Panda-Core/SecurityData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Panda_Core
+{
+    [DataContract]
+    public class SecurityData
+    {
+        [DataMember]
+        public string SecId { get; set; }
+
+        [DataMember]
+        public double Price { get; set; }
+
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+    }
+}
